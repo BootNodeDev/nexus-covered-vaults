cf477313d553967a55015cbcea0b1cc76434d140
Gerardo Nardelli <patitonardelli@gmail.com>
2022-09-23 11:29:38 -0300

    Enable solidity optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-contract-sizer";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
 
 export default config;
