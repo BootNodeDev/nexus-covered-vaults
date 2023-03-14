e646caea0789d75e5884d16c824511e74c430c8a
Gerardo Nardelli <patitonardelli@gmail.com>
2023-03-14 15:48:20 -0300

    rename ReentrancyGuard import

--- contracts/CoverManager.sol
+++ contracts/CoverManager.sol
@@ -2,10 +2,9 @@
 pragma solidity 0.8.17;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import { ICover, BuyCoverParams, PoolAllocationRequest } from "./interfaces/ICover.sol";
 import { IPool } from "./interfaces/IPool.sol";
 
