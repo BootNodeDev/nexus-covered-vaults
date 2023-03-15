e7b730e9eac45a4d660df81c4955009dd496cb15
Gerardo Nardelli <patitonardelli@gmail.com>
2023-03-15 10:41:59 -0300

    Upgrade solhint and prettier solidity version

--- contracts/interfaces/ICoverNFT.sol
+++ contracts/interfaces/ICoverNFT.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface ICoverNFT is IERC721 {
   function mint(address to, uint256 tokenId) external;
